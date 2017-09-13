63527793b307cd8daefd866369521ca898e9123a
Murat Tasan <murat.tasan@sympoz.com>
2017-09-13 13:59:27 -0600

    fix --<name>

--- awscli/R/s3api.R
+++ awscli/R/s3api.R
@@ -1,5 +1,7 @@
+## TODO: normalize filepath? tilde (i.e. ~) expansion doesn't seem to work with the sys::exec calls.
+
 s3api_list_objects <- function(bucket, ...) {
-    x <- exec_aws_cli("s3api", "list-objects-v2", "--bucket" = bucket, ...)
+    x <- exec_aws_cli("s3api", "list-objects-v2", "bucket" = bucket, ...)
     if(jsonlite::validate(x$stdout)) {
         jsonlite::fromJSON(x$stdout)[[1]] %>% tibble::as_tibble()
     } else {
@@ -9,14 +11,14 @@ s3api_list_objects <- function(bucket, ...) {
 
 s3api_put_object <- function(filepath, bucket, key) {
     exec_aws_cli("s3api", "put-object",
-                 "--bucket" = bucket,
-                 "--key" = key,
-                 "--body" = filepath)
+                 "bucket" = bucket,
+                 "key" = key,
+                 "body" = filepath)
 }
 
 s3api_get_object <- function(bucket, key, filepath) {
     exec_aws_cli("s3api", "get-object",
-                 "--bucket" = bucket,
-                 "--key" = key,
+                 "bucket" = bucket,
+                 "key" = key,
                  filepath)
 }
